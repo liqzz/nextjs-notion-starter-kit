51b7ae2dfcdf4edf04974d67aad27b7ee49cf5f6
qingzhaoli <github-personal@github.com>
2024-05-06 10:54:13 +0800

    update condif

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '81a5b64addfc43ce8ac8556bd4b28834',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
